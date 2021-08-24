c34c422af1bcca691329fcc8ce69f1107021e13e
Dhakal29 <dhakal.bek@gmail.com>
2021-08-24 17:42:26 +0545

    updated-with-transitions(works_only_with_server_or_host)

--- RioApartmentRental/js/transition.js
+++ RioApartmentRental/js/transition.js
@@ -1,3 +1,8 @@
 
-const swup = new Swup();
-  
+let options = {
+    url: window.location.pathname + window.location.search,
+    method: 'GET',
+    data: null,
+    header: {},
+};
+const swup = new Swup(options);
