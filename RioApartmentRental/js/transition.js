b2e55f152a1e5d35df18715c90bf0e7b0e67cf67
Dhakal29 <dhakal.bek@gmail.com>
2021-08-25 12:39:31 +0545

    transition only with xampp or localhost

--- RioApartmentRental/js/transition.js
+++ RioApartmentRental/js/transition.js
@@ -1,4 +1,4 @@
-
+// window.getBrowser().webNavigation.loadURI (msg, 0, null, null, null );
 let options = {
     url: window.location.pathname + window.location.search,
     method: 'GET',
@@ -6,3 +6,10 @@ let options = {
     header: {},
 };
 const swup = new Swup(options);
+
+
+// {
+//     window.history.pushState(
+//         {url:t||window.location.href.split(window.location.hostname)[1],random:Math.random(),source:"swup"},
+//         document.getElementsByTagName("title")[0].innerText,
+//         t||window.location.href.split(window.location.hostname)[1])||window.location.pathname.split(/.*[\/|\\]/)[1]}
